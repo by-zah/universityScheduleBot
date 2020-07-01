30c63eef5eec96a201bf47be0da5d792684296d5
Zakhar <zahgeek@gmail.com>
2020-07-01 12:26:47 +0300

    init commit

---
+++ src/main/java/ua/khnu/Bot.java
@@ -0,0 +1,38 @@
+package ua.khnu;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingCommandBot {
+    public static final String TOKEN_FILE_PATH = "token.txt";
+    private final String botToken;
+
+    public Bot() throws IOException {
+        botToken = Files.readString(Paths.get(TOKEN_FILE_PATH));
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
