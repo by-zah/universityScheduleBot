bcd80fad595b28e7facbf346f471e4517f6a9edd
Zakhar <zahgeek@gmail.com>
2020-07-01 13:06:31 +0300

    for server testing

--- src/main/java/ua/khnu/Bot.java
+++ src/main/java/ua/khnu/Bot.java
@@ -1,7 +1,12 @@
 package ua.khnu;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,6 +14,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingCommandBot {
+    private static final Logger LOG = LogManager.getLogger(Bot.class);
     public static final String TOKEN_FILE_PATH = "token.txt";
     private final String botToken;
 
@@ -18,7 +24,12 @@ public class Bot extends TelegramLongPollingCommandBot {
 
     @Override
     public void onUpdatesReceived(List<Update> updates) {
-
+        updates.forEach(update ->{
+            SendMessage message = new SendMessage();
+            message.setText("!!!");
+            message.setChatId(update.getMessage().getChatId());
+            sendMessageToUser(message,update.getMessage().getFrom(),null);
+        });
     }
 
     @Override
@@ -34,5 +45,21 @@ public class Bot extends TelegramLongPollingCommandBot {
     @Override
     public String getBotToken() {
         return botToken;
+    }
+
+    private void sendMessageToUser(SendMessage message, User receiver, User sender) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            LOG.error(e);
+        }
+    }
+
+    private void replyToUser(SendMessage message, User user, String messageText) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            LOG.error(e);
+        }
     }
 }
