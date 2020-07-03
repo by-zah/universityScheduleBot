4a023e9346f58abfa142bf66ced51a894a2ccc9d
Zakhar <zahgeek@gmail.com>
2020-07-03 13:06:20 +0300

    add new commands to init

--- src/main/java/ua/khnu/BotInitializer.java
+++ src/main/java/ua/khnu/BotInitializer.java
@@ -2,19 +2,24 @@ package ua.khnu;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import ua.khnu.commans.NonCommandCommand;
 import ua.khnu.commans.StartCommand;
+import ua.khnu.commans.SubscribeCommand;
+import ua.khnu.commans.UnSubscribeCommand;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class BotInitializer {
     private static final Logger LOG = LogManager.getLogger(BotInitializer.class);
     private final TelegramLongPollingCommandBot bot;
-
+    private final List<Long> subscribers;
     public BotInitializer() {
         this.bot = new Bot(new NonCommandCommand());
+        subscribers = new CopyOnWriteArrayList<>();
         registerCommands();
     }
 
@@ -31,5 +36,7 @@ public class BotInitializer {
 
     private void registerCommands() {
         bot.register(new StartCommand());
+        bot.register(new SubscribeCommand(subscribers));
+        bot.register(new UnSubscribeCommand(subscribers));
     }
 }
