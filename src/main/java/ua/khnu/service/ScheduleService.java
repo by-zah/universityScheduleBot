4df3d848ece6c5f9dbdce405df66d8126b3f3114
Zakhar <zahgeek@gmail.com>
2020-07-19 20:20:22 +0300

    fix bug

--- src/main/java/ua/khnu/service/ScheduleService.java
+++ src/main/java/ua/khnu/service/ScheduleService.java
@@ -7,6 +7,7 @@ import ua.khnu.entity.Day;
 import ua.khnu.entity.Lesson;
 import ua.khnu.entity.Schedule;
 import ua.khnu.repository.ScheduleRepository;
+import ua.khnu.util.Converter;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -32,28 +33,27 @@ public class ScheduleService {
     public DayAndIndex getCurrentDayWithIndex() {
         Schedule schedule = repository.getSchedule().orElseThrow(IllegalStateException::new);
         LocalDateTime date = LocalDate.now(ZoneId.of(TIME_ZONE_ID)).atStartOfDay();
-        Optional<Day> dayOpt;
-        do {
-            dayOpt = getSpecifiedWorkingDay(date.getDayOfWeek(), schedule);
+
+        Optional<Lesson> lessonOpt = Optional.empty();
+        Day day = null;
+        while (!lessonOpt.isPresent()) {
+            Optional<Day> dayOpt = getSpecifiedWorkingDay(date.getDayOfWeek(), schedule);
+            while (!dayOpt.isPresent()) {
+                dayOpt = getSpecifiedWorkingDay(date.getDayOfWeek(), schedule);
+                date = date.plusDays(1);
+            }
+            day = dayOpt.get();
+            day.getLessons().sort(Lesson::compareTo);
+            lessonOpt = getNextLesson(day, date, LocalDateTime.now(ZoneId.of(TIME_ZONE_ID)));
             date = date.plusDays(1);
-        } while(!dayOpt.isPresent());
-        Day day = dayOpt.get();
-        Lesson lesson = getNextLesson(day,date,LocalDateTime.now(ZoneId.of(TIME_ZONE_ID)));
-        return new DayAndIndex(day,day.getLessons().indexOf(lesson));
+        }
+        return new DayAndIndex(day, day.getLessons().indexOf(lessonOpt.get()));
     }
 
-    private Lesson getNextLesson(Day workingDay, LocalDateTime nextLessonDate, LocalDateTime now) {
-        Lesson nextLesson = null;
-        LocalDateTime nextLessonDateTime;
-        for (Lesson lesson : workingDay.getLessons()) {
-            nextLessonDateTime = nextLessonDate.plusHours(lesson.getStartHour());
-            nextLessonDateTime = nextLessonDateTime.plusMinutes(lesson.getStartMin());
-            if (nextLessonDateTime.compareTo(now) > 0) {
-                nextLesson = lesson;
-                break;
-            }
-        }
-        return nextLesson;
+    private Optional<Lesson> getNextLesson(Day workingDay, LocalDateTime nextLessonDate, LocalDateTime now) {
+        return workingDay.getLessons().stream()
+                .filter(lesson -> Converter.lessonToStartDateTime(lesson, nextLessonDate.getDayOfWeek()).compareTo(now) > 0)
+                .findFirst();
     }
 
     private Optional<Day> getSpecifiedWorkingDay(DayOfWeek dayOfWeek, Schedule schedule) {
